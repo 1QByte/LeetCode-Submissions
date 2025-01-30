32172a745847b0edbcce63c16559e406a2f7f850
Iqbra Kurniawan <140613782+1QByte@users.noreply.github.com>
2025-01-31 05:37:40 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isThree(self, n):
+        div = 0
+
+        for i in (1, n + 1):
+            if n % i == 0:
+                div += 1
+
+        if div == 3:
+            return True
+        else:
+            return False
+        
+
+        
