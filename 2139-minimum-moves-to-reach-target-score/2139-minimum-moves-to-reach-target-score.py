e74b0f4147d5d57c9fb1293ef0591e3a01f8b1a9
Iqbra Kurniawan <140613782+1QByte@users.noreply.github.com>
2025-01-30 21:48:10 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minMoves(self, target, maxDoubles):
+        result = 0
+
+        while target != 1:
+            if maxDoubles > 0 and target % 2 == 0:
+                target //= 2
+                maxDoubles -= 1
+            else:
+                target -= 1
+            result += 1
+
+        return result
