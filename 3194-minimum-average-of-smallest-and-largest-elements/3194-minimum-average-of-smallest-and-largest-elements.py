912ebeee40489713e00cfa43eb9b68ca5fc101a2
Iqbra Kurniawan <140613782+1QByte@users.noreply.github.com>
2025-03-01 05:04:26 +0700

    Time: 0 ms (100%), Space: 17.7 MB (67.52%) - LeetHub

--- 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.py
+++ 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.py
@@ -3,14 +3,9 @@ class Solution:
         nums.sort()
         averages = []
 
-        i = len(nums) - 1
         while len(nums) != 0:
-            temp = (nums[0] + nums[i]) / 2
-
+            temp = (nums.pop(0) + nums.pop()) / 2
             averages.append(temp)
-            nums.pop(0)
-            nums.pop(i)
-            i -= 2
 
-        return sum(averages) / len(averages)
+        return min(averages)
 
