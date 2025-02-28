92acb8493e3ada420470b7f23f77ac5c14915764
Iqbra Kurniawan <140613782+1QByte@users.noreply.github.com>
2025-03-01 05:35:45 +0700

    Time: 9 ms (16.84%), Space: 12.5 MB (24.23%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -2,7 +2,7 @@ class Solution(object):
     def findTheDifference(self, s, t):
         for i in t:
             if i in s:
-                s = s.replace(i,"")
+                s = s.replace(i, "", 1)
             else:
                 return i
         
