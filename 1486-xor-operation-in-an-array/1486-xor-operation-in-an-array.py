ff20b7d23c79fc252f5918d9243be84eb9311c27
Iqbra Kurniawan <140613782+1QByte@users.noreply.github.com>
2025-03-01 05:42:42 +0700

    Time: 0 ms (100%), Space: 17.7 MB (81.78%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.py
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.py
@@ -1,10 +1,10 @@
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
         nums = []
-        result 
+        result = 0
 
         for i in range(n):
-            nums.append(start + 2 * 1)
+            nums.append(start + 2 * i)
 
         for num in nums:
             result ^= num
