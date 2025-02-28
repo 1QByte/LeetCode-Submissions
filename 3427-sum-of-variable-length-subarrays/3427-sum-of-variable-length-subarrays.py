b9036d987212dc38c51b2bae3597226d0bb7b2a7
Iqbra Kurniawan <140613782+1QByte@users.noreply.github.com>
2025-03-01 05:40:24 +0700

    Time: 23 ms (5.16%), Space: 17.7 MB (95.92%) - LeetHub

--- 3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.py
+++ 3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.py
@@ -1,13 +1,11 @@
 class Solution:
     def subarraySum(self, nums: List[int]) -> int:
-        n = len(nums)
-        result = nums[0]
+        result = 0
 
-        i, j = 0, 1
-        while j < n:
-            result += (nums[i] + nums[j])
-            
-            i, j = i + 1, j + 1
+        for i in range(len(nums)):
+            start = max(0, i - nums[i])
+            for j in range(start, i + 1):
+                result += nums[j]
 
         return result
         
