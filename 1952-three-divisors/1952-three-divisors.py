83576c434c2f3972c2388b4fbe6f6f7161f1832f
Iqbra Kurniawan <140613782+1QByte@users.noreply.github.com>
2025-03-02 09:23:26 +0700

    Time: 7 ms (32.42%), Space: 12.7 MB (58.9%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.py
+++ 1952-three-divisors/1952-three-divisors.py
@@ -2,7 +2,7 @@ class Solution(object):
     def isThree(self, n):
         div = 0
 
-        for i in (1, n + 1):
+        for i in range(1, n + 1):
             if n % i == 0:
                 div += 1
 
